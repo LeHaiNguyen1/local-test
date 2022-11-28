f1f9c0c415dfe52f72bd87d46ad022115cafc283
Nguyễn Lê Hải <nguyenlehai@Nguyens-MacBook-Pro.local>
2022-11-29 00:40:16 +0700

    Update about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,104 +2,93 @@ import React, { useState } from 'react'
 import './index.scss'
 import Popup from '../Popup';
 import Category from '../Category';
-
+import Pagination from "react-custom-pagination";
+import Caro from "../../assets/images/caro.svg";
+import flapbird from "../../assets/images/flapbird.svg";
+import chichbongbong from "../../assets/images/chichbongbong.svg";
+import Goldegg from "../../assets/images/goldegg.svg";
+import Primary from "../../assets/images/primary.svg"
+import ReactPaginate from 'react-paginate';
 
 export default function Redeem() {
     const [showPopup, setShowPopup] = useState(false);
     const togglePopup = () => {
         setShowPopup(prevState => !prevState);
     }
-    return (
-        <section id="redeem" className="redeem">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="redeem__grid">
-                            <div className="redeem__item">
-                                <div className="redeem__item img">
-                                    <img src={require('../../assets/images/caro.svg').default} onClick={togglePopup} />
-                                </div>
-                                <div className="redeem__item title">
-                                    <h3>Chess Caro</h3>
-                                </div>
-                                <div className="redeem__item desc">
-                                    <img src={require('../../assets/images/primary.svg').default} />
-                                    <p>1289 Play times</p>
-                                </div>
+    const posts = [
+        { id: "1", title: "Chess Caro", img: Caro, icon: Primary, titleicon: "1289 Play times" },
+        { id: "2", title: "Flap Bird", img: flapbird, icon: Primary, titleicon: "1289 Play times" },
+        { id: "3", title: "Flap Bird", img: chichbongbong, icon: Primary, titleicon: "1289 Play times" },
+        { id: "4", title: "Flap Bird", img: Goldegg, icon: Primary, titleicon: "1289 Play times" },
+        { id: "5", title: "Flap Bird", img: flapbird, icon: Primary, titleicon: "1289 Play times" }
+    ];
 
-                            </div>
-                        </div>
-                        <div className="redeem__grid">
-                            <div class="redeem__item">
-                                <div class="redeem__item img">
-                                    <img src={require('../../assets/images/chichbongbong.svg').default} onClick={togglePopup} />
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(4);
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (number) => {
+        setCurrentPage(number);
+    };
+    return (
+        <>
+            <section id="redeem" className="redeem">
+                <div className='container'>
+                    <div className='row'>
+                        {currentPosts.map((item) => {
+                            return (
+                                <div className='col-6' key={item.id}>
+                                    <div class="redeem__grid">
+                                        <div class="redeem__item">
+                                            <div class="redeem__item img">
+                                                <img src={item.img} alt="" className='w-100' />
+                                            </div>
+                                            <div class="redeem__item title">
+                                                <h3 className='text-light'>Flap Bird</h3>
+                                            </div>
+                                            <div class="redeem__item desc text-light">
+                                                <img src={item.icon} alt="" className='' />
+                                                {item.titleicon}
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="redeem__item title">
-                                    <h3>Bubbles</h3>
-                                </div>
-                                <div class="redeem__item desc">
-                                    <img src={require('../../assets/images/primary.svg').default} />
-                                    <p>1289 Play times</p>
-                                </div>
-                            </div>
-                        </div>
+                            );
+                        })}
                     </div>
-                    <div class="col">
-                        <div class="redeem__grid">
-                            <div class="redeem__item">
-                                <div class="redeem__item img">
-                                    <img src={require('../../assets/images/flapbird.svg').default} onClick={togglePopup} />
-                                </div>
-                                <div class="redeem__item title">
-                                    <h3>Flap Bird</h3>
-                                </div>
-                                <div class="redeem__item desc">
-                                    <img src={require('../../assets/images/primary.svg').default} />
-                                    <p>1289 Play times</p>
-                                </div>
+                </div>
+                <div className='col-sm-12 d-flex justify-content-center align-items-center text-center"'>
+                    <div class="container">
+                        <div class="row d-flex justify-content-center align-items-center">
+                            <div class="col text-end">
+                                <a href="#" class="pagination__prev">
+                                    <i class="fas fa-caret-left"></i>
+                                </a>
                             </div>
-                        </div>
-                        <div class="redeem__grid">
-                            <div class="redeem__item">
-                                <div class="redeem__item img">
-                                    <img src={require('../../assets/images/goldegg.svg').default} onClick={togglePopup} />
-                                </div>
-                                <div class="redeem__item title">
-                                    <h3>Gold Egg</h3>
-                                </div>
-                                <div class="redeem__item desc">
-                                    <img src={require('../../assets/images/primary.svg').default} />
-                                    <p>1289 Play times</p>
-                                </div>
+                            <div class="col">
+                                <Pagination
+                                    totalPosts={posts.length}
+                                    postsPerPage={postsPerPage}
+                                    paginate={paginate}
+                                    view={5}
+                                    showLast={true}
+                                    showFirst={true}
+                                />
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 d-flex justify-content-center align-items-center text-center">
-                        <div class="pagination">
-                            <a href="#" class="pagination__prev">
-                                <i class="fas fa-caret-left"></i>
-                            </a>
-                            <a href="#" class="pagination active">1</a>
-                            <a href="#">2</a>
-                            <a href="#">3</a>
-                            <a href="#">4</a>
-                            <a href="#">...</a>
-                            <a href="#" class="pagination__next">
-                                <i class="fas fa-caret-right"></i>
-                            </a>
+                            <div class="col">
+                                <a href="#" class="pagination__next">
+                                    <i class="fas fa-caret-right"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {showPopup ?
-                    <div className='text-left'>
-                        <Popup closePopup={togglePopup} />
-                    </div>
-
-                    : null
-                }
                 <Category />
-
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
