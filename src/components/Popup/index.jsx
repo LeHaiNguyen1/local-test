2c124232dc4a5a7220ec7ae66880276a89ec4fbd
グエン　レ　ハイ <lehainguyen0802@gmail.com>
2022-12-01 21:50:03 +0700

    update page and design UI_UX

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,43 +1,25 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './index.scss'
-import Caro from "../../assets/images/caro.svg";
-import Primary from "../../assets/images/primary.svg";
-import flapbird from "../../assets/images/flapbird.svg";
-import chichbongbong from "../../assets/images/chichbongbong.svg";
-import Goldegg from "../../assets/images/goldegg.svg";
-
-
-const posts = [
-    { id: "1", title: "Pasukan Tentara", img: Caro, titleicon: "Test1" },
-    { id: "2", title: "Flap Bird", img: flapbird, titleicon: "Test2" },
-    { id: "3", title: "Flap Bird", img: chichbongbong, titleicon: "Test3" },
-    { id: "4", title: "Flap Bird", img: Goldegg, titleicon: "Test4" },
-    { id: "5", title: "Flap Bird", img: flapbird, titleicon: "Test5" }
-];
-export default function Popup(props) {
-
-
+export default function Popup({ detailId, toggle}) {    
     return (
         <section id='popup' className='popup'>
             <div className='container'>
                 <div className='row'>
-                            <div className='popup' key={posts.id}>
-                                <div className='popup_inner'>
-                                    <span onClick={props.closePopup}>x</span>
-                                    <div className='img d-flex justify-content-center align-content-center'>
-                                        <img src={props.dataFromParent} style={{width:"20%"}}/>
-                                    </div>
-                                    <div
-                                        className='row title d-flex justify-content-center align-items-center text-center'>
-                                        <h1 className='text-light fs-4'>{posts.title}</h1>
-                                        <p>{props.dataFromParent.titleicon}</p>
-                                        <div className="modal-content-block__btn">
-                                            <button>Play now</button>
-                                        </div>
-                                    </div>
+                    <div className='popup' key={detailId.id}>
+                        <div className='popup_inner'>
+                            <span onClick={toggle}>x</span>
+                            <div className='img d-flex justify-content-center align-content-center'>
+                                <img src={detailId.img} className="w-100" />
+                            </div>
+                            <div className='row title d-flex justify-content-center align-items-center text-center'>
+                                <div className='h1 text-light fs-4'>{detailId.title}</div>
+                                <p>{detailId.content}</p>
+                                <div className="modal-content-block__btn">
+                                    <button>Play now</button>
                                 </div>
                             </div>
-
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
