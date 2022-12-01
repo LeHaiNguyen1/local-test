01c76fdbf0b543a4592a3a12c0e16292e09c5d64
グエン　レ　ハイ <lehainguyen0802@gmail.com>
2022-12-01 07:03:19 +0700

    Update page and popup

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,51 +1,40 @@
 import React, { useState } from 'react'
 import './index.scss'
-import Popup from '../Popup';
 import Category from '../Category';
-import Pagination from "react-custom-pagination";
 import Caro from "../../assets/images/caro.svg";
 import flapbird from "../../assets/images/flapbird.svg";
 import chichbongbong from "../../assets/images/chichbongbong.svg";
 import Goldegg from "../../assets/images/goldegg.svg";
 import Primary from "../../assets/images/primary.svg"
-import ReactPaginate from 'react-paginate';
+import SweetPagination from "sweetpagination";
+import Popup from "../Popup";
 
+const posts = [
+    { id: "1", title: "Chess Caro", img: Caro, icon: Primary, titleicon: "1289 Play times" },
+    { id: "2", title: "Flap Bird", img: flapbird, icon: Primary, titleicon: "1289 Play times" },
+    { id: "3", title: "Flap Bird", img: chichbongbong, icon: Primary, titleicon: "1289 Play times" },
+    { id: "4", title: "Flap Bird", img: Goldegg, icon: Primary, titleicon: "1289 Play times" },
+    { id: "5", title: "Flap Bird", img: flapbird, icon: Primary, titleicon: "1289 Play times" }
+];
 export default function Redeem() {
+    const [currentPageData, setCurrentPageData] = useState([]);
+    const [detailId, setDetailId] = useState()
     const [showPopup, setShowPopup] = useState(false);
     const togglePopup = () => {
         setShowPopup(prevState => !prevState);
     }
-    const posts = [
-        { id: "1", title: "Chess Caro", img: Caro, icon: Primary, titleicon: "1289 Play times" },
-        { id: "2", title: "Flap Bird", img: flapbird, icon: Primary, titleicon: "1289 Play times" },
-        { id: "3", title: "Flap Bird", img: chichbongbong, icon: Primary, titleicon: "1289 Play times" },
-        { id: "4", title: "Flap Bird", img: Goldegg, icon: Primary, titleicon: "1289 Play times" },
-        { id: "5", title: "Flap Bird", img: flapbird, icon: Primary, titleicon: "1289 Play times" }
-    ];
-
-
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(4);
-
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-    const paginate = (number) => {
-        setCurrentPage(number);
-    };
     return (
         <>
             <section id="redeem" className="redeem">
                 <div className='container'>
                     <div className='row'>
-                        {currentPosts.map((item) => {
+                        {currentPageData.map((item) => {
                             return (
-                                <div className='col-6' key={item.id}>
+                                <div className='col-6' >
                                     <div class="redeem__grid">
-                                        <div class="redeem__item">
+                                        <div class="redeem__item" key={item.id}>
                                             <div class="redeem__item img">
-                                                <img src={item.img} alt="" className='w-100' />
+                                                <img src={item.img} alt="" className='w-100' onClick={()=>setDetailId(item.img)}/>
                                             </div>
                                             <div class="redeem__item title">
                                                 <h3 className='text-light'>Flap Bird</h3>
@@ -57,35 +46,37 @@ export default function Redeem() {
                                         </div>
                                     </div>
                                 </div>
+
                             );
+
                         })}
                     </div>
-                </div>
-                <div className='col-sm-12 d-flex justify-content-center align-items-center text-center"'>
-                    <div class="container">
-                        <div class="row d-flex justify-content-center align-items-center">
-                            <div class="col text-end">
-                                <a href="#" class="pagination__prev">
-                                    <i class="fas fa-caret-left"></i>
-                                </a>
-                            </div>
-                            <div class="col">
-                                <Pagination
-                                    totalPosts={posts.length}
-                                    postsPerPage={postsPerPage}
-                                    paginate={paginate}
-                                    view={5}
-                                    showLast={true}
-                                    showFirst={true}
-                                />
-                            </div>
-                            <div class="col">
-                                <a href="#" class="pagination__next">
-                                    <i class="fas fa-caret-right"></i>
-                                </a>
+                    <div className='col-sm-12 d-flex justify-content-center align-items-center text-center"'>
+                        <div className="container">
+                            <div className="row d-flex justify-content-center align-items-center">
+                                <div className="col text-end">
+                                    <a href="#" className="pagination__prev">
+                                        <i className="fas fa-caret-left"></i>
+                                    </a>
+                                </div>
+                                <div className="col">
+                                    <SweetPagination
+                                        currentPageData={setCurrentPageData}
+                                        getData={posts}
+                                        dataPerPage={4}
+                                        navigation={true}
+                                        getStyle={'style-custom'}
+                                    />
+                                </div>
+                                <div className="col">
+                                    <a href="#" className="pagination__next">
+                                        <i className="fas fa-caret-right"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    {detailId && <Popup dataFromParent={detailId}/>}
                 </div>
                 <Category />
             </section>
--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.scss'
+import Caro from "../../assets/images/caro.svg";
+import Primary from "../../assets/images/primary.svg";
+import flapbird from "../../assets/images/flapbird.svg";
+import chichbongbong from "../../assets/images/chichbongbong.svg";
+import Goldegg from "../../assets/images/goldegg.svg";
 
+
+const posts = [
+    { id: "1", title: "Pasukan Tentara", img: Caro, titleicon: "Test1" },
+    { id: "2", title: "Flap Bird", img: flapbird, titleicon: "Test2" },
+    { id: "3", title: "Flap Bird", img: chichbongbong, titleicon: "Test3" },
+    { id: "4", title: "Flap Bird", img: Goldegg, titleicon: "Test4" },
+    { id: "5", title: "Flap Bird", img: flapbird, titleicon: "Test5" }
+];
 export default function Popup(props) {
+
+
     return (
         <section id='popup' className='popup'>
             <div className='container'>
                 <div className='row'>
-                    <div className='popup'>
-                        <div className='popup_inner'>
-                            <span onClick={props.closePopup}>x</span>
-                            <div className='img d-flex justify-content-center align-content-center'>
-                                <img src={require('../../assets/images/banner-popup.svg').default} />
-                            </div>
-                            <div className='row title d-flex justify-content-center align-items-center text-center'>
-                                <h1 className='text-light fs-4'>Pasukan Tentara</h1>
-                                <p>This game is very fun telling the story of a wise prince. this game is 3d and very visualization </p>
-                                <div classname="modal-content-block__btn">
-                                    <button>Play now</button>
+                            <div className='popup' key={posts.id}>
+                                <div className='popup_inner'>
+                                    <span onClick={props.closePopup}>x</span>
+                                    <div className='img d-flex justify-content-center align-content-center'>
+                                        <img src={props.dataFromParent} style={{width:"20%"}}/>
+                                    </div>
+                                    <div
+                                        className='row title d-flex justify-content-center align-items-center text-center'>
+                                        <h1 className='text-light fs-4'>{posts.title}</h1>
+                                        <p>{props.dataFromParent.titleicon}</p>
+                                        <div className="modal-content-block__btn">
+                                            <button>Play now</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </section>
