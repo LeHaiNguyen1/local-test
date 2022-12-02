66eeb98e0166a46e65d791ac72359414874b0883
グエン　レ　ハイ <lehainguyen0802@gmail.com>
2022-12-02 21:09:11 +0700

    update title

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,11 +11,11 @@ import SweetPagination from "sweetpagination";
 import Popup from "../Popup";
 
 const posts = [
-    { id: "1", title: "Category ",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: Caro, icon: Primary, titleicon: "1289 Play times" },
-    { id: "2", title: "Caro",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: flapbird, icon: Primary, titleicon: "1289 Play times" },
-    { id: "3", title: "Bubble",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: chichbongbong, icon: Primary, titleicon: "1289 Play times" },
+    { id: "1", title: "Chess Caro",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: Caro, icon: Primary, titleicon: "1289 Play times" },
+    { id: "2", title: "Flap Bird",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: flapbird, icon: Primary, titleicon: "1289 Play times" },
+    { id: "3", title: "Bubbles",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: chichbongbong, icon: Primary, titleicon: "1289 Play times" },
     { id: "4", title: "Goldegg",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: Goldegg, icon: Primary, titleicon: "1289 Play times" },
-    { id: "5", title: "Primary",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: therock, icon: Primary, titleicon: "1289 Play times" }
+    { id: "5", title: "The Rock",content: "This game is very fun telling the story of a wise prince. this game is 3d and very visualization", img: therock, icon: Primary, titleicon: "1289 Play times" }
 ];
 
 export default function Redeem() {
